d9b3d00cbb73c02d80ab28eb64f95fee449c757d
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-11-14 13:18:31 -0600

    created product columns in product model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,44 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+     // Id Column 
+     id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // Product_name Column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // Price Column 
+    price: {
+      type: DataTypes.DECIMAL, 
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    // Stock Column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    // Category_id Column
+    category_id: {
+      type: DataTypes.INTEGER,
+      // Category Model Id Reference 
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
