3e3db932e1626740d8a59b2d9602cbd9ad93f418
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-11-14 15:17:01 -0600

    finished product find one route

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -29,7 +29,33 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
   // be sure to include its associated Category and Tag data
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+    .then(dbProductModel => {
+      if (!dbProductModel) {
+        res.status(404).json({ message: 'No product found with this id'}); 
+        return; 
+      }
+      res.json(dbProductModel);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
