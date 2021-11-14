a61fd579596d677323ecda6a5cee012952114a22
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-11-14 13:28:31 -0600

    created producttag columns in producttag model

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,31 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // Id Columns
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // Product_id Column
+    product_id: {
+      type: DataTypes.INTEGER,
+      // references the product model's id 
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    // Tag_id Column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // Tag Model Id Reference 
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
